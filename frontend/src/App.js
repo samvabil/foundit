1e854af4adb8a82eaed864387d0075fe2943a0f9
SuveshaU <suvesha2002@gmail.com>
2024-11-16 15:44:27 -0600

    Lostitems

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+<<<<<<< HEAD
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import FoundItems from './pages/FoundItems';
@@ -32,6 +33,33 @@ function App() {
         </Routes>
       </div>
     </Router>
+=======
+import UploadImage from './components/UploadImage';
+import UserPrompt from './components/UserPrompt';
+import LostItem from './pages/LostItems'; // Import the LostItem component
+
+function App() {
+  const [imageUrl, setImageUrl] = useState('');
+  
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+
+      <h1>Image Upload App</h1>
+
+      <div className="home-page">
+        <h1>Welcome to the Image Upload App</h1>
+        {/* Pass the setImageUrl function as a prop */}
+        <UploadImage setImageUrl={setImageUrl} />
+        {/* Conditionally render UserPrompt if imageUrl is available */}
+        {imageUrl && <UserPrompt imageUrl={imageUrl} />}
+      </div>
+
+      <div className="lost-items-section">
+        <LostItem /> {/* Render the LostItem component */}
+      </div>
+    </div>
+>>>>>>> af880fc (Lostitems)
   );
 }
 
